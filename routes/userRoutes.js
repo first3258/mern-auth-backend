cd6bc6b919da41661e595d368be2e75cb52fcb1c
Pitchayuth Rattanamonkol <firstpitchayuth@gmail.com>
2023-05-18 12:43:00 +0700

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const router = express.Router()
+import {
+    authUser,
+    resgisterUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile 
+} from '../controllers/userControllers.js'
+import {protect} from '../middleware/authMiddleware.js'
+
+router.post('/', resgisterUser)
+router.post('/auth', authUser)
+router.post('/logout', logoutUser)
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
+
+export default router
